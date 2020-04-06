a01f2aec8849fbeb6497699996c17e03b2c16bff
Erik Prenzlin <eprenzlin@gmail.com>
2020-04-06 21:35:08 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -8,5 +8,12 @@ end
 
 attr_accessor :title, :schedule, :description
 
+def initialize(title, schedule, description)
+  @title = title 
+  @schedule = schedule 
+  @description = description
+  @@all << self 
+end 
+
 
 end
