740c5d3e35fd5d97b1c260e1c78a381857dd8b43
Erik Prenzlin <eprenzlin@gmail.com>
2020-04-06 21:37:29 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,17 +1,19 @@
 class Course
-
-@@all = []
-
-def self.all
-  @@all
-end
-
-
-attr_accessor :title, :schedule, :description
-
-def initialize
-  @@all << self
-end
-
-
+ 
+  attr_accessor :title, :schedule, :description
+ 
+  @@all = []
+ 
+  def initialize
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+ 
+  def self.reset_all
+    @@all.clear
+  end
+ 
 end
